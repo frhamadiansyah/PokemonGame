aa649ce8e9c0fb42739f917d9daa5a5c7e6036af
Fandrian Rhamadiansyah <fandrian.rhamadiansyah@gmail.com>
2024-08-09 10:36:52 +0700

    Initial Commit

---
+++ PokemonGame/PokemonGameApp.swift
@@ -0,0 +1,20 @@
+//
+//  PokemonGameApp.swift
+//  PokemonGame
+//
+//  Created by Fandrian Rhamadiansyah on 09/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PokemonGameApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
